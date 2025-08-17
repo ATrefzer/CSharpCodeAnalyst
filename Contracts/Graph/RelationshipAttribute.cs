6b7d244c2c2b86ac126a1ff4c091333cdf5896ec
Andreas Trefzer <36333177+ATrefzer@users.noreply.github.com>
2025-08-17 17:15:09 +0200

    Call type refactoring (#9)
    Collect attributes like base, instance etc for a call.
    This information is used for the follow incoming calls heuristics.

---
+++ Contracts/Graph/RelationshipAttribute.cs
@@ -0,0 +1,43 @@
+﻿namespace Contracts.Graph;
+
+[Flags]
+public enum RelationshipAttribute : uint
+{
+    None = 0,
+
+    // Call specific attributes
+    IsBaseCall = 1,
+    IsStaticCall = 2,
+    IsThisCall = 4,
+    IsInstanceCall = 8,
+    IsExtensionMethodCall = 16,
+}
+
+
+public static class RelationshipAttributeExtensions
+{
+    private static readonly List<RelationshipAttribute> Flags = Enum.GetValues(typeof(RelationshipAttribute))
+        .Cast<RelationshipAttribute>()
+        .Where(r => r != RelationshipAttribute.None)
+        .ToList();
+
+    public static string FormatAttributes(this RelationshipAttribute attr)
+    {
+        if (attr == RelationshipAttribute.None)
+        {
+            return string.Empty;
+        }
+
+        var attributes = new List<string>();
+        foreach (var flag in Flags)
+        {
+            if (attr.HasFlag(flag))
+            {
+                attributes.Add(flag.ToString());
+            }
+        }
+
+        return "[" + string.Join(", ", attributes) + "]";
+    }
+
+}
