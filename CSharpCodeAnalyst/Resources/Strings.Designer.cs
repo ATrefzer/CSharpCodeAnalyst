e95b67789b2dffb5d00a5228dc1dd227df5d9e41
ATrefzer <36333177+ATrefzer@users.noreply.github.com>
2025-10-07 16:09:24 +0200

    Cleanup tooltip

--- CSharpCodeAnalyst/Resources/Strings.Designer.cs
+++ CSharpCodeAnalyst/Resources/Strings.Designer.cs
@@ -1619,14 +1619,11 @@ namespace CSharpCodeAnalyst.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 
-        ///Logical operations: space = AND, &apos;|&apos; = OR
+        ///   Looks up a localized string similar to Logical operations: space = AND, &apos;|&apos; = OR
         ///Search for type = type:xxx
         ///Search for internal code elements = source:intern      
         ///Search for external code elements = source:extern
-        ///Search with resharper style = Use at least one uppercase character in a search term.
-        ///
-        ///.
+        ///Search with resharper style = Use at least one uppercase character in a search term..
         /// </summary>
         public static string SearchPattern_Tooltip {
             get {
