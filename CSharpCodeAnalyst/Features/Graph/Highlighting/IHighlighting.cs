da226b9bbf0c9281c51e9c9e2df31de3b7532ff2
ATrefzer <36333177+ATrefzer@users.noreply.github.com>
2026-06-19 20:20:37 +0200

    Remove msagl

--- CSharpCodeAnalyst/Features/Graph/Highlighting/IHighlighting.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Msagl.Drawing;
-
-namespace CSharpCodeAnalyst.Features.Graph.Highlighting;
-
-internal interface IHighlighting
-{
-    void Highlight(IGraphViewerHighlighting graphViewer, IViewerObject? viewerObject,
-        CodeGraph.Graph.CodeGraph? codeGraph);
-}
